e7d8d3b76f3f46ae85f5a0983b20a56c235d5f83
Woodlouse2002 <117754217+Woodlouse2002@users.noreply.github.com>
2022-12-04 14:33:06 +0000

    Update cleaning.r

--- functions/cleaning.r
+++ functions/cleaning.r
@@ -1,3 +1,4 @@
+#function to clean raw data 
 cleaning <- function(data){
   data %>%
     select(-starts_with("Delta")) %>%
@@ -6,7 +7,7 @@ cleaning <- function(data){
     remove_empty(c("rows", "cols"))
 }
 
-
+#function to filter clean data to be relevant for ANCOVA
 Adelie_Clean <- function(penguins_clean){
   penguins_clean %>%
     filter(species == "Adelie Penguin (Pygoscelis adeliae)", !is.na(sex) & !is.na(culmen_depth_mm) & !is.na(body_mass_g))
